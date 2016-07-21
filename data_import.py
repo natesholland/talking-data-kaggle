c5cadc9fed95d53aa345f39f23094eaf755c1e27
Nate Holland <nateh@spiceworks.com>
2016-07-21 16:09:36 -0500

    more data import, adding another table

--- data_import.py
+++ data_import.py
@@ -7,20 +7,12 @@ conn = psycopg2.connect("host='localhost' port='5432' dbname='kaggle'")
 
 cur = conn.cursor()
 
+# -------------------------phone_brand_device_model----------------------------
 dropTableString = """
-DROP TABLE phone_brand_device_model;
+DROP TABLE IF EXISTS phone_brand_device_model;
 """
 
 cur.execute(dropTableString)
-
-databaseSelectString = """
-SELECT table_schema,table_name
-FROM information_schema.tables
-ORDER BY table_schema,table_name;
-"""
-
-# cur.execute('SELECT current_database();')
-# print(cur.fetchall())
 
 createTableString = """
 CREATE TABLE phone_brand_device_model (
@@ -36,8 +28,35 @@ f = open(appPath + '/data/phone_brand_device_model.csv', 'r')
 f.readline()
 
 cur.copy_from(f, 'phone_brand_device_model', sep=',')
-cur.execute('select * from phone_brand_device_model limit 1')
-print(cur.fetchall())
+# cur.execute('select * from phone_brand_device_model limit 1')
+# print(cur.fetchall())
+
+# --------------------------------events---------------------------------------
+
+dropTableString = """
+DROP TABLE IF EXISTS events;
+"""
+
+cur.execute(dropTableString)
+
+createTableString = """
+CREATE TABLE events (
+    event_id        integer,
+    device_id       varchar(40),
+    timestamp       timestamp,
+    longitude       float,
+    latitude        float
+);
+"""
+
+cur.execute(createTableString)
+
+f = open(appPath + '/data/events.csv', 'r')
+f.readline()
+
+cur.copy_from(f, 'events', sep=',')
+
+
 
 conn.commit()
 conn.close()
