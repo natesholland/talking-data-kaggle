c2e428d3349d2e27d4ce01a56dc67fe9bcb2e8d5
Nate Holland <nateh@spiceworks.com>
2016-07-22 14:17:52 -0500

    basic sketch of a random forrest classifier

--- sketch.py
+++ sketch.py
@@ -1,7 +1,9 @@
+import datetime
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import log_loss
 
 # Helpful debugger line copied from here: https://gist.github.com/obfusk/208597ccc64bf9b436ed
 # import code; code.interact(local=dict(globals(), **locals()))
@@ -24,6 +26,8 @@ pbd = map_column(pbd, 'phone_brand')
 pbd = map_column(pbd, 'device_model')
 
 train = pd.read_csv("data/gender_age_train.csv", dtype={'device_id': np.str})
+labels = sorted(train['group'].unique())
+print(labels)
 train = map_column(train, 'group')
 train = train.drop(['gender'], axis=1)
 train = train.drop(['age'], axis=1)
@@ -34,9 +38,38 @@ train.fillna(-1, inplace=True)
 X_tr = train[['phone_brand', 'device_model', 'counts']].values
 y_tr = train['group'].values
 
+test = pd.read_csv("data/gender_age_test.csv", dtype={'device_id': np.str})
+test = pd.merge(test, pbd, how='left', on='device_id', left_index=True)
+test = pd.merge(test, events_small, how='left', on='device_id', left_index=True)
+test.fillna(-1, inplace=True)
 
-recognizer = RandomForestClassifier(4, max_depth=5)
+X_test = test[['phone_brand', 'device_model', 'counts']].values
+
+recognizer = RandomForestClassifier(10, max_depth=5)
 recognizer.fit(X_tr, y_tr)
+
+prediction = recognizer.predict_proba(X_test)
+
+
+def write_submission_file(test, prediction):
+    now = datetime.datetime.now()
+    sub_file = 'submission_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+    f = open(sub_file, 'w')
+    f.write('device_id,F23-,F24-26,F27-28,F29-32,F33-42,F43+,M22-,M23-26,M27-28,M29-31,M32-38,M39+\n')
+    total = 0
+    test_val = test['device_id'].values
+    for i in range(len(test_val)):
+        str1 = str(test_val[i])
+        for j in range(12):
+            str1 += ',' + str(prediction[i][j])
+        str1 += '\n'
+        total += 1
+        f.write(str1)
+    f.close()
+
+write_submission_file(test, prediction)
+
+# import code; code.interact(local=dict(globals(), **locals()))
 
 
 
