ca933a5344998254513d493bc933c6c06dfaca10
Nate Holland <nateh@spiceworks.com>
2016-07-25 19:44:46 -0500

    exploding out all the feature vectors

--- sketch.py
+++ sketch.py
@@ -10,8 +10,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import log_loss
 
-NUM_TREES = 100
-TREE_DEPTH = 12
+NUM_TREES = 500
+TREE_DEPTH = 20
+NUM_THREADS = 4
 
 # Helpful debugger line copied from here: https://gist.github.com/obfusk/208597ccc64bf9b436ed
 # import code; code.interact(local=dict(globals(), **locals()))
@@ -67,38 +68,56 @@ app_events = pd.merge(app_events, app_labels, how='left', on='app_id', left_inde
 app_events['ones'] = 1
 print('beginning app events pivoting...')
 app_events_pivoted = app_events.pivot_table(index='event_id', columns='label_id', values='ones')
-category_columns = app_events_pivoted.columns.values
+category_columns = list(app_events_pivoted.columns.values)
 app_events_pivoted['event_id'] = app_events_pivoted.index
 print('beginning app events summing...')
 for col in category_columns:
     app_events_pivoted[col] = app_events_pivoted.groupby(['event_id'])[col].transform('sum')
+print('beginning app events merging...')
+# events = pd.merge(events, app_events_pivoted, how='left', on='event_id', left_index=True)
+events_to_device_id = events[['device_id', 'event_id']]
+foobar  = pd.merge(app_events_pivoted, events_to_device_id, how='left', on='event_id', left_index=True)
+print('beginning summing over categories...')
+for col in category_columns:
+    foobar[col] = foobar.groupby(['device_id'])[col].transform('sum')
+foobar.fillna(0, inplace=True)
+foobar.drop('event_id', axis=1, inplace=True)
 
-foobar = pd.merge(events, app_events_pivoted, how='left', on='event_id', left_index=True)
-import code; code.interact(local=dict(globals(), **locals()))
-
+print('importing phone device models...')
 pbd = pd.read_csv("data/phone_brand_device_model.csv", dtype={'device_id': np.str})
 pbd.drop_duplicates('device_id', inplace=True)
 pbd = map_column(pbd, 'phone_brand')
 pbd = map_column(pbd, 'device_model')
 
+print('setting up test table')
 train = pd.read_csv("data/gender_age_train.csv", dtype={'device_id': np.str})
-labels = sorted(train['group'].unique())
-print(labels)
 train = map_column(train, 'group')
 train = train.drop(['gender'], axis=1)
 train = train.drop(['age'], axis=1)
 train = pd.merge(train, pbd, how='left', on='device_id', left_index=True)
 train = pd.merge(train, events_small, how='left', on='device_id', left_index=True)
+train = pd.merge(train, foobar, how='left', on='device_id', left_index=True)
 train.fillna(-1, inplace=True)
+train.drop_duplicates(subset=['device_id'], inplace=True)
 
 print("importing test data...")
 
 test = pd.read_csv("data/gender_age_test.csv", dtype={'device_id': np.str})
 test = pd.merge(test, pbd, how='left', on='device_id', left_index=True)
 test = pd.merge(test, events_small, how='left', on='device_id', left_index=True)
+test = pd.merge(test, foobar, how='left', on='device_id', left_index=True)
 test.fillna(-1, inplace=True)
+test.drop_duplicates(subset=['device_id'], inplace=True)
 
-X_test = test[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names].values
+f = open('data/train_features.csv', 'w')
+train.to_csv(path_or_buf=f)
+f.close()
+
+f = open('data/test_features.csv', 'w')
+test.to_csv(path_or_buf=f)
+f.close()
+
+X_test = test[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names + category_columns].values
 
 # This is what we can use for cross validation
 result = train_test_split(train)
@@ -112,15 +131,15 @@ if (os.name == 'nt'):
     X_val = val_data[:, 2:]
     y_val = val_data[:, 1]
 else:
-    X_tr = train_data[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names].values
+    X_tr = train_data[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names + category_columns].values
     y_tr = train_data['group'].values
-    X_val = val_data[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names].values
+    X_val = val_data[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names + category_columns].values
     y_val = val_data['group'].values
 
 # import code; code.interact(local=dict(globals(), **locals()))
 
 print('fitting data...')
-recognizer = RandomForestClassifier(NUM_TREES, max_depth=TREE_DEPTH)
+recognizer = RandomForestClassifier(NUM_TREES, max_depth=TREE_DEPTH, verbose=1, n_jobs=NUM_THREADS)
 recognizer.fit(X_tr, y_tr)
 
 print('prediting data...')
@@ -133,10 +152,10 @@ else:
 
 print("Log loss: " + str(ll))
 
-X_tr = train[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names].values
+X_tr = train[['phone_brand', 'device_model', 'counts', 'mean_longitude', 'mean_latitude'] + hour_column_names + category_columns].values
 y_tr = train['group'].values
 
-recognizer = RandomForestClassifier(NUM_TREES, max_depth=TREE_DEPTH)
+recognizer = RandomForestClassifier(NUM_TREES, max_depth=TREE_DEPTH, verbose=1, n_jobs=NUM_THREADS)
 recognizer.fit(X_tr, y_tr)
 prediction = recognizer.predict_proba(X_test)
 
