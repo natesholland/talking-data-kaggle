69ee783488abe560ef8d508045e6d2999522d91b
Nate Holland <nateh@spiceworks.com>
2016-08-19 11:22:01 -0500

    adding gbtree script

---
+++ classify-boosting-gbtree.py
@@ -0,0 +1,128 @@
+# Based on yibo's R script and JianXiao's Python script.
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from scipy import sparse
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, scale
+from sklearn.decomposition import TruncatedSVD, SparsePCA
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn.feature_selection import SelectPercentile, f_classif, chi2
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import log_loss
+
+# Create bag-of-apps in character string format
+# first by event
+# then merge to generate larger bags by device
+
+# import code; code.interact(local=dict(globals(), **locals()))
+
+path = "data/"
+
+##################
+#  Train and Test
+##################
+print("# Generate Train and Test")
+
+train = pd.read_csv("data/gender_age_train.csv",
+                    dtype={'device_id': np.str})
+train.drop(["age", "gender"], axis=1, inplace=True)
+
+test = pd.read_csv("data/gender_age_test.csv",
+                   dtype={'device_id': np.str})
+test["group"] = np.nan
+
+split_len = len(train)
+
+# Group Labels
+Y = train["group"]
+lable_group = LabelEncoder()
+Y = lable_group.fit_transform(Y)
+device_id = test["device_id"]
+
+
+FLS = pd.read_csv(path+"fls.csv",
+                  dtype={'device_id': np.str})
+
+###################
+# User-Item Feature
+###################
+print("# User-Item-Feature")
+
+device_ids = FLS["device_id"].unique()
+feature_cs = FLS["feature"].unique()
+
+data = np.ones(len(FLS))
+dec = LabelEncoder().fit(FLS["device_id"])
+row = dec.transform(FLS["device_id"])
+col = LabelEncoder().fit_transform(FLS["feature"])
+sparse_matrix = sparse.csr_matrix(
+    (data, (row, col)), shape=(len(device_ids), len(feature_cs)))
+
+sparse_matrix = sparse_matrix[:, sparse_matrix.getnnz(0) > 0]
+
+##################
+#      Data
+##################
+
+train_row = dec.transform(train["device_id"])
+train_sp = sparse_matrix[train_row, :]
+
+test_row = dec.transform(test["device_id"])
+test_sp = sparse_matrix[test_row, :]
+
+X_train, X_val, y_train, y_val = train_test_split(
+    train_sp, Y, train_size=.90, random_state=10)
+
+##################
+#   Feature Sel
+##################
+print("# Feature Selection")
+selector = SelectPercentile(f_classif, percentile=100)
+
+selector.fit(X_train, y_train)
+
+X_train = selector.transform(X_train)
+X_val = selector.transform(X_val)
+
+train_sp = selector.transform(train_sp)
+test_sp = selector.transform(test_sp)
+
+print("# Num of Features: ", X_train.shape[1])
+
+##################
+#  Build Model
+##################
+
+dtrain = xgb.DMatrix(X_train, y_train)
+dvalid = xgb.DMatrix(X_val, y_val)
+
+params = {
+    "objective": "multi:softprob",
+    "num_class": 12,
+    "booster": "gbtree",
+    "max_depth": 6,
+    "eval_metric": "mlogloss",
+    "eta": 0.02,
+    "silent": 1,
+    "alpha": 3,
+    "min_child_weight": 2.2,
+    "gamma": 0,
+    "subsample": 0.7,
+    "colsample_bytree": 0.7
+}
+watchlist = [(dtrain, 'train'), (dvalid, 'eval')]
+gbm = xgb.train(params, dtrain, 1000, evals=watchlist,
+                early_stopping_rounds=40, verbose_eval=True)
+
+y_pre = gbm.predict(xgb.DMatrix(test_sp))
+
+# Write results
+result = pd.DataFrame(y_pre, columns=lable_group.classes_)
+result["device_id"] = device_id
+result = result.set_index("device_id")
+result.to_csv('fine_tune.csv', index=True,
+          index_label='device_id')
